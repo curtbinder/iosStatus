17a809b009292afd378e766141ab100e31f3361b
Curt Binder <curt.binder@gmail.com>
2014-02-15 22:59:23 -0500

    Copyright notice officially added to the files.
    Licensed under the Creative Commons ShareAlike 4.0 License.
    See copyright.txt file for more details.

--- iosStatus/RAHost.h
+++ iosStatus/RAHost.h
@@ -1,10 +1,12 @@
-//
-//  RAHost.h
-//  ReefAngelStatus
-//
-//  Created by Curt Binder on 10/19/13.
-//  Copyright (c) 2013 Curt Binder. All rights reserved.
-//
+/*
+ * Copyright (c) 2013 by Curt Binder (http://curtbinder.info)
+ *
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-sa/4.0/deed.en_US
+ */
+
 
 #import <Foundation/Foundation.h>
 
